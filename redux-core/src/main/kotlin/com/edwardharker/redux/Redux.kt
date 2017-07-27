6f1a18bd288c73e37716107929066ba419916674
Edward <edwardharks@yahoo.co.uk>
2017-07-27 19:35:13 +0100

    Creates interface for Store

--- redux-core/src/main/kotlin/com/edwardharker/redux/Redux.kt
+++ redux-core/src/main/kotlin/com/edwardharker/redux/Redux.kt
@@ -1,2 +1,14 @@
 package com.edwardharker.redux
 
+interface Action
+
+interface State
+
+class Store<out S : State> {
+
+    fun dispatch(action: Action) = Unit
+
+    fun subscribe(listener: (S) -> Unit) = Unit
+
+    fun getState(): S = TODO()
+}
---
+++ redux-core/src/test/kotlin/com/edwardharker/redux/StoreTest.kt
@@ -0,0 +1,11 @@
+package com.edwardharker.redux
+
+import org.junit.Test
+
+class StoreTest {
+
+    @Test
+    fun someTest() {
+
+    }
+}
