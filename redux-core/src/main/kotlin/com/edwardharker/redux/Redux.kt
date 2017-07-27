e157e7dc474d58e46f7b6f150cd8721a8f5ac004
Edward <edwardharks@yahoo.co.uk>
2017-07-27 20:46:15 +0100

    Basic Store implementation

--- redux-core/src/main/kotlin/com/edwardharker/redux/Redux.kt
+++ redux-core/src/main/kotlin/com/edwardharker/redux/Redux.kt
@@ -1,14 +1,34 @@
 package com.edwardharker.redux
 
+import java.util.*
+
 interface Action
 
 interface State
 
-class Store<out S : State> {
+class Store<out S : State>(private val reducer: (S, Action) -> S, private val initialState: S) {
 
-    fun dispatch(action: Action) = Unit
+    private val listeners = HashSet<(S) -> Unit>()
 
-    fun subscribe(listener: (S) -> Unit) = Unit
+    private var state: S = initialState
 
-    fun getState(): S = TODO()
+    fun dispatch(action: Action) {
+        state = reducer(initialState, action)
+
+        for (listener in listeners) {
+            listener(state)
+        }
+    }
+
+    fun subscribe(listener: (S) -> Unit): () -> Unit {
+        listeners.add(listener)
+
+        val unsubscriber = fun() {
+            listeners.remove(listener)
+        }
+
+        return unsubscriber
+    }
+
+    fun getState(): S = state
 }
--- redux-core/src/test/kotlin/com/edwardharker/redux/StoreTest.kt
+++ redux-core/src/test/kotlin/com/edwardharker/redux/StoreTest.kt
@@ -1,11 +1,66 @@
 package com.edwardharker.redux
 
+import org.hamcrest.CoreMatchers.equalTo
+import org.junit.Assert.assertThat
 import org.junit.Test
 
 class StoreTest {
 
     @Test
-    fun someTest() {
+    fun dispatchActionProvidesActionToReducer() {
+        var reducedAction: Action? = null
 
+        val reducer = fun(state: FakeState, action: Action): FakeState {
+            reducedAction = action
+            return state
+        }
+
+        val store = Store(reducer, FakeState())
+
+        store.dispatch(FakeAction)
+        assertThat(reducedAction, equalTo(FakeAction as Action))
     }
+
+    @Test
+    fun dispatchActionProvidesInitialStateToReducer() {
+        var oldState: State? = null
+
+        val reducer = fun(state: FakeState, action: Action): FakeState {
+            oldState = state
+            return state
+        }
+
+        val expected = FakeState()
+        val store = Store(reducer, expected)
+        store.dispatch(FakeAction)
+        assertThat(oldState, equalTo(expected as State))
+    }
+
+    @Test
+    fun dispatchActionNotifiesListenersWithNewState() {
+        val reducer = fun(state: FakeState, action: Action) = state
+
+        val expected = object : FakeState() {}
+        val store = Store(reducer, expected)
+
+        var observedState: FakeState? = null
+        store.subscribe { state -> observedState = state }
+        store.dispatch(FakeAction)
+
+        assertThat(observedState, equalTo(expected as FakeState))
+    }
+
+    @Test
+    fun getStateReturnsCurrentState() {
+        val expected = object : FakeState() {}
+
+        val reducer = fun(state: FakeState, action: Action) = expected
+        val store = Store(reducer, FakeState())
+
+        store.dispatch(FakeAction)
+        assertThat(store.getState(), equalTo(expected as FakeState))
+    }
+
+    private open class FakeState : State
+    private object FakeAction : Action
 }
