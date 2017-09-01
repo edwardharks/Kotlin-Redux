0054482aff7d4ae9d8081535439898e40e247e90
Edward <edwardharks@yahoo.co.uk>
2017-09-01 18:50:59 +0100

    A better redux implementation copied from some other apps i was testing it out with

--- redux-core/src/main/kotlin/com/edwardharker/redux/Redux.kt
+++ redux-core/src/main/kotlin/com/edwardharker/redux/Redux.kt
@@ -4,31 +4,27 @@ import java.util.*
 
 interface Action
 
-interface Store<out S> {
+class Store<S>(private val reducer: (S, Action) -> S, initialState: S) {
 
-    fun dispatch(action: Action)
-
-    fun subscribe(listener: (S) -> Unit): () -> Unit
-
-    fun getState(): S
-}
-
-class DefaultStore<out S>(private val reducer: (S, Action) -> S, initialState: S) : Store<S> {
-
-    private var state: S = initialState
+    var state: S = initialState
+        private set
     private val listeners = HashSet<(S) -> Unit>()
     private var isDispatching = false
+    private val lock = Object()
 
-    override fun dispatch(action: Action) {
-        if (isDispatching) {
-            throw IllegalStateException("Reducers may not dispatch actions.")
-        }
 
-        try {
-            isDispatching = true
-            state = reducer(state, action)
-        } finally {
-            isDispatching = false
+    fun dispatch(action: Action) {
+        synchronized(lock) {
+            if (isDispatching) {
+                throw IllegalStateException("Reducers may not dispatch actions.")
+            }
+
+            try {
+                isDispatching = true
+                state = reducer(state, action)
+            } finally {
+                isDispatching = false
+            }
         }
 
         for (listener in listeners) {
@@ -36,7 +32,8 @@ class DefaultStore<out S>(private val reducer: (S, Action) -> S, initialState: S
         }
     }
 
-    override fun subscribe(listener: (S) -> Unit): () -> Unit {
+
+    fun subscribe(listener: (S) -> Unit): () -> Unit {
         listeners.add(listener)
         listener(state)
 
@@ -47,38 +44,7 @@ class DefaultStore<out S>(private val reducer: (S, Action) -> S, initialState: S
         return unsubscriber
     }
 
-    override fun getState(): S = state
-
     companion object {
-        fun <S> create(reducer: (S, Action) -> S, initialState: S): Store<S> = DefaultStore(reducer, initialState)
-    }
-}
-
-class ThreadSafeStore<out S>(reducer: (S, Action) -> S, initialState: S) : Store<S> {
-
-    private val defaultStore = DefaultStore(reducer, initialState)
-
-    private val lock = Object()
-
-    override fun dispatch(action: Action) {
-        synchronized(lock) {
-            defaultStore.dispatch(action)
-        }
-    }
-
-    override fun subscribe(listener: (S) -> Unit): () -> Unit {
-        synchronized(lock) {
-            return defaultStore.subscribe(listener)
-        }
-    }
-
-    override fun getState(): S {
-        synchronized(lock) {
-            return defaultStore.getState()
-        }
-    }
-
-    companion object {
-        fun <S> create(reducer: (S, Action) -> S, initialState: S): Store<S> = ThreadSafeStore(reducer, initialState)
+        fun <S> create(reducer: (S, Action) -> S, initialState: S): Store<S> = Store(reducer, initialState)
     }
 }
